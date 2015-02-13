f21eccf0ae96a922990ca615df10e48e52e4574d
arunachalam25 <arunachalam25@gmail.com>
2015-02-13 10:30:00 +0530

    second commit

--- web.js
+++ web.js
@@ -4,4 +4,6 @@ var app = express();
  
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT ||  3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
